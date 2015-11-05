280e828cbeb5ab5ac2ecec63ea5d8bbb95320712
Mike Wallace <mfwallace@gmail.com>
2015-11-04 22:39:08 -0500

    Added methods to update class variables based on the properties.

--- src/io/miti/shortstop/server/Config.java
+++ src/io/miti/shortstop/server/Config.java
@@ -6,8 +6,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public final class Config
 {
@@ -71,5 +73,78 @@ public final class Config
     final String sPort = props.getProperty("http.port");
     port = Utility.parseStringAsInt(sPort, DEFAULT_PORT);
     port = Math.max(20, Math.min(20_000, port));
+    
+    // Check if the user can download files
+    canDownloadFiles = Utility.parseStringAsBoolean(
+        props.getProperty("file.download.allowed"), false);
+    
+    // Check if the user can download all extensions, or just some
+    final String exts = props.getProperty("file.extensions.filter");
+    if ((exts == null) || exts.trim().isEmpty()) {
+      canDownloadFiles = false;
+    } else {
+      if (exts.equals("*")) {
+        canDownloadAllExtensions = true;
+      } else {
+        final StringTokenizer st = new StringTokenizer(exts, ",");
+        if (st.hasMoreTokens()) {
+          allowedExtensions = new HashSet<String>(10);
+          while (st.hasMoreTokens()) {
+            final String str = st.nextToken().trim();
+            if (!str.isEmpty()) {
+              allowedExtensions.add(str.toLowerCase());
+            }
+          }
+        }
+      }
+    }
+    
+    // Get the directory for downloading files
+    fileDirectory = props.getProperty("file.directory");
+    if ((fileDirectory == null) || fileDirectory.trim().isEmpty()) {
+      // No directory specified, so users cannot download files
+      canDownloadFiles = false;
+      fileDirectory = null;
+    }
+  }
+  
+  
+  /**
+   * Return whether users can download any files at all.
+   * 
+   * @return whether the server supports file retrieval
+   */
+  public boolean canDownloadFiles() {
+    // If these conditions are met, then it's possible that the user may be
+    // able to download a given file
+    return (canDownloadFiles && (fileDirectory != null) &&
+            (canDownloadAllExtensions || ((allowedExtensions != null) && !allowedExtensions.isEmpty())));
+  }
+  
+  
+  /**
+   * Return whether a user can download a particular extension.
+   * 
+   * @param ext the file extension
+   * @return whether that extension can be downloaded
+   */
+  public boolean canDownloadExtension(final String ext) {
+    // Null input returns false
+    if (ext == null) {
+      return false;
+    }
+    
+    return (canDownloadFiles() && (canDownloadAllExtensions ||
+            ((allowedExtensions != null) && allowedExtensions.contains(ext.toLowerCase()))));
+  }
+  
+  
+  /**
+   * Return the file directory.
+   * 
+   * @return the file directory, or null if not in the properties file
+   */
+  public String getFileDirectory() {
+    return fileDirectory;
   }
 }
