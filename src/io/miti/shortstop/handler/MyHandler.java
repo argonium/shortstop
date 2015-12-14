c78325c7baccafcdb9656753dc0b3955b25fc193
Mike Wallace <mfwallace@gmail.com>
2015-12-14 14:55:44 -0500

    Fixed a test method.

--- src/io/miti/shortstop/handler/MyHandler.java
+++ src/io/miti/shortstop/handler/MyHandler.java
@@ -18,7 +18,7 @@ public final class MyHandler {
   
   public static Response addStrings(final Request req) {
     final Response resp = new Response(200);
-    String sum = req.getParameterByKey("val1") + req.getParameterByKey("val2");
+    String sum = req.getTemplateVariable("val1") + req.getTemplateVariable("val2");
     String msg = "{\"sum\": \"" + sum + "\"}";
     resp.setJsonContentType().setBody(msg);
     return resp;
